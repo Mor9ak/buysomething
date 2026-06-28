2045bf206ae6b7a8748552b31b75df9e4c1a10f2
Mor9ak <skricheery0rik@gmail.com>
2026-06-28 21:33:46 +0300

    style (header) minor style fix refactor (header) added semantic tag feat (project) added imgPlaceholder.png

--- widgets/header/Header.tsx
+++ widgets/header/Header.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className={'flex flex-row justify-between uniPadding border-b-2 border-borderCol w-full'}>
+        <header className={'flex flex-row justify-between uniPadding border-b-2 border-borderCol w-full mb-5 md:mb-9'}>
             <h2 className={'hidden w-fit text-nowrap md:flex text-2xl text-mainTextCol'}>Some store</h2>
-            <ul className={'flex flex-row w-full justify-end gap-5 text-textCol text-xl'}>
-                {links.map((link, index) => (
-                    <li key={index}>
-                        <Link href={link.href}>{link.title}</Link>
-                    </li>
-                ))}
-            </ul>
+            <nav>
+                <ul className={'flex flex-row w-full justify-evenly md:justify-end gap-5 text-textCol text-xl'}>
+                    {links.map((link, index) => (
+                        <li key={index}>
+                            <Link href={link.href}>{link.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     );
 }
