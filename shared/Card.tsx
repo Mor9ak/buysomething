737faf988c6fc619e9482e43c7eede1099810cff
Mor9ak <skricheery0rik@gmail.com>
2026-06-28 21:31:55 +0300

    refactor (card) changes in Card.tsx - Changed interface - Change styles - Added checks of props

--- shared/Card.tsx
+++ shared/Card.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import Image from 'next/image'
 
 export interface CardProps {
     title: string;
-    text: string;
-    children?: React.ReactNode;
+    text?: string;
+    imgSrc?: string;
     description?: string;
 }
 
 const Card = (props: CardProps) => {
     return (
-        <div className={'flex flex-col bg-secBgCol border-2 rounded-b-md w-fit uniPadding gap-5'}>
-            <h3 className={'text-mainTextCol text-2xl'}>{props.title}</h3>
-            <h4 className={'text-textCol text-xl'}>{props.text}</h4>
-            {props.children}
-            <p className={'text-secTextCol'}>{props.description}</p>
+        <div className={'flex flex-col bg-cardBgdCol justify-center items-center uniBorder rounded-lg w-fit max-w-[44vw] md:max-w-[25vw] uniPadding gap-5 shadow-lg shadow-accentCol/20 text-wrap'}>
+            <h3 className={'text-mainTextCol text-2xl text-center'}>{props.title}</h3>
+            {props.text && <h4 className={'text-textCol text-xl'}>{props.text}</h4>}
+            {props.imgSrc && <div className={'flex justify-center items-center w-[40vw] box-border md:w-[20vw] aspect-square'}>
+                <Image src={props.imgSrc} alt={props.title} width={100} height={100} className={'w-full h-full rounded-lg object-cover'}/>
+            </div>}
+            {props.description && <p className={'text-secTextCol'}>{props.description}</p>}
         </div>
     );
 }
