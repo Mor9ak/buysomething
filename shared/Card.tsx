8ce3339a1ec9f997261849020dc048ebe6acf2fc
Mor9ak <skricheery0rik@gmail.com>
2026-07-01 19:45:20 +0300

    style (card): minor style changes: - fix sizes - used lint style (project): change bottom paddings

--- shared/Card.tsx
+++ shared/Card.tsx
@@ -10,12 +10,15 @@ export interface CardProps {
 
 const Card = (props: CardProps) => {
     return (
-        <div className={'flex flex-col bg-cardBgdCol justify-center items-center uniBorder rounded-lg w-fit max-w-[44vw] md:max-w-[25vw] uniPadding gap-5 shadow-lg shadow-accentCol/20 text-wrap'}>
+        <div
+            className={'flex flex-col bg-cardBgdCol justify-center items-center uniBorder rounded-lg w-[40vw] md:w-[30vw] max-w-[44vw] md:max-w-[25vw] uniPadding gap-5 shadow-lg shadow-accentCol/20'}>
             <h3 className={'text-mainTextCol text-2xl text-center'}>{props.title}</h3>
-            {props.text && <h4 className={'text-textCol text-xl'}>{props.text}</h4>}
-            {props.imgSrc && <div className={'flex justify-center items-center w-[40vw] box-border md:w-[20vw] aspect-square'}>
-                <Image src={props.imgSrc} alt={props.title} width={100} height={100} className={'w-full h-full rounded-lg object-cover'}/>
-            </div>}
+            {props.text && <h4 title={props.text} className={'text-textCol text-xl truncate max-w-[34vw] md:max-w-[20vw]'}>{props.text}</h4>}
+            {props.imgSrc &&
+                <div className={'flex justify-center items-center w-[40vw] box-border md:w-[20vw] aspect-square'}>
+                    <Image src={props.imgSrc} alt={props.title} width={100} height={100}
+                           className={'w-full h-full rounded-xl object-cover'}/>
+                </div>}
             {props.description && <p className={'text-secTextCol'}>{props.description}</p>}
         </div>
     );
