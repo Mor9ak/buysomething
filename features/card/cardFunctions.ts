e39fb6909c4f1cdf9bba34818770c6ba1089f28e
Mor9ak <skricheery0rik@gmail.com>
2026-07-07 20:16:17 +0300

    refactor(card): user-friendly change

--- features/card/cardFunctions.ts
+++ features/card/cardFunctions.ts
@@ -1,16 +1,25 @@
 import cards from '@/widgets/CardHolder/data/cards.json';
 
 export function isCardExist(id: number | string) {
-    console.log(`id: ${id}`);
+    console.log('\nisCardExist');
+    console.log(`ID - ${id}`);
+    console.log('type of ID - ' + typeof id);
+
     const strId = typeof id === 'string' ? id : id.toString();
     return cards.some(card => card.id === strId);
 }
 
 export function getCard(id: number | string) {
+    console.log('\ngetCard');
+    console.log(`ID - ${id}`);
+    console.log('type of ID - ' + typeof id);
+
     const strId = typeof id === 'string' ? id : id.toString();
     if (isCardExist(strId)) {
+        console.log ('success');
         return cards.find((card) => card.id === strId);
     }
     console.log(`Card with id ${strId} not found`);
+    console.log ('failure');
     return null;
 }
