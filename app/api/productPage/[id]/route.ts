707d5f869f25300120a7994ff6661fdeff0f160a
Mor9ak <skricheery0rik@gmail.com>
2026-07-08 20:12:14 +0300

    refactor (card): - change cardFunctions.ts directory - change path in route.ts

--- app/api/productPage/[id]/route.ts
+++ app/api/productPage/[id]/route.ts
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import {getCard} from "@/features/card/cardFunctions";
+import {getCard} from "@/lib/cardFunctions";
 
 export async function GET(request: Request, {params}: { params: Promise<{ id: string }> }) {
     const {id} = await params;
