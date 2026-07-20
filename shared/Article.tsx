768765293ab9c71f6c4f0084d1ba5ce57a358669
Mor9ak <skricheery0rik@gmail.com>
2026-07-20 20:03:46 +0300

    style(shared): delete text shadow from Article.tsx

--- shared/Article.tsx
+++ shared/Article.tsx
@@ -9,7 +9,7 @@ export interface ArticleProps {
 const Article = (props: ArticleProps) => {
     return (
         <article className={`flex flex-col w-[90vw] text-center justify-center items-center font-bold text-mainTextCol gap-5 ${props.style}`}>
-            <h2 className={'text-shadow-sm text-shadow-accentCol text-2xl'}>{props.title}</h2>
+            <h2 className={'text-2xl'}>{props.title}</h2>
             {props.text && <h4 className={'text-xl'}>{props.text}</h4>}
         </article>
     );
