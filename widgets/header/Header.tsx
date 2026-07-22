75947e4252303c980bc97e8c696e19bb87d1ef7b
Mor9ak <skricheery0rik@gmail.com>
2026-07-22 12:55:49 +0300

    style(header): change mobile logo view

--- widgets/header/Header.tsx
+++ widgets/header/Header.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header className={'flex flex-row justify-between uniPadding border-b-2 border-borderCol w-full mb-5 md:mb-9 bg-secBgCol'}>
+        <header className={'flex flex-row justify-between items-center uniPadding border-b-2 border-borderCol w-full mb-5 md:mb-9 bg-secBgCol'}>
             <Link href={'/'} >
-                <h2 className={'hidden w-fit text-nowrap md:flex text-2xl text-mainTextCol'}>Some store</h2>
+                <h2 className={'w-fit text-nowrap flex text-2xl text-mainTextCol'}>Some store</h2>
             </Link>
             <nav>
                 <ul className={'flex flex-row w-full justify-evenly md:justify-end gap-5 text-textCol text-xl'}>
